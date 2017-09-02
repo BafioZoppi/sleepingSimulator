9216e8425ee613dca5ef1abee6dd3de87cf8fa13
Bafio Zoppi <Bafio Zoppi>
2017-09-02 10:36:19 +0200

    Aggiungere i file di progetto.

---
+++ src/ofApp.cpp
@@ -0,0 +1,110 @@
+#include "ofApp.h"
+
+int GAME = 0;
+int MENU = 1;
+int status = MENU;
+int selected = 0;
+string menu[3] = { "GIOCA", "IMPOSTAZIONI", "ESCI" };
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+	bigFont.load("Helvetica-Regular.ttf", 20);
+	smallFont.load("Helvetica-Regular.ttf", 16);
+	ofSetEscapeQuitsApp(false);
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+	ofBackground(0);
+	ofSetColor(255);
+
+	if (status == MENU) {
+		bigFont.drawString("SLEEPING SIMULATOR", 100, 100);
+		for (int i = 0; i < 3; i++) {
+			if (selected == i) {
+				ofSetColor(0, 255, 0);
+				smallFont.drawString(menu[i], 100, 200 + i * 100);
+				ofSetColor(255);
+			}
+			else
+				smallFont.drawString(menu[i], 100, 200 + i * 100);
+		}
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+	if (key == OF_KEY_RETURN) {
+		if (status == MENU) {
+			if (selected == 0)
+				status = GAME;
+			else if (selected == 2)
+				OF_EXIT_APP(0);
+		}
+	}
+	else if (key == OF_KEY_ESC)
+		status = MENU;
+	else if (key == OF_KEY_DOWN)
+		selected++;
+	else if (key == OF_KEY_UP)
+		selected--;
+
+	if (selected > 2)
+		selected = 0;
+	if (selected < 0)
+		selected = 2;
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
